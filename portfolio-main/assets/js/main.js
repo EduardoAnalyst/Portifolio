b31a4486e01658be995dfb357d3d03fc8128048c
Jalison Eduardo da Silva Macedo <123666176+EduardoAnalyst@users.noreply.github.com>
2024-04-28 00:24:56 -0300

    Add files via upload

---
+++ portfolio-main/assets/js/main.js
@@ -0,0 +1,95 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".digitalhouse",
+  `Na Leroy Merlin eu atuei como Atendente depois Assistente de Supply onde desempenhei um cargo de análise de dados logisticos e de vendas e atualmente sou assistente de customer experience, apesar de estar fora da minha área de atuação
+  a experiência do cliente me mostrou o quão importante é os dados para as pessoas que nos promovem diariamente.`,
+  "Customer Experience",
+  "Leroy Merlin",
+  "Jan 2021 - Atual"
+);
+
+hoverChangeExperience(
+  ".zuplae",
+  `Configurava e administrava a captura de dados dos clientes do Santander, Cielo, Crefisa e Shell. Essa captura de Dados era básicamente os cookies , além de ter a mentalidade voltada pra segurança e privacidade do cliente eu configurava todo o Design das campanhas de Retargeting e Remarketing dentro do funil de vendas e também apoiava como suporte técnico da empresa`,
+  "Estagiário de Engenharia de Dados",
+  "Etag Digital",
+  "Mar 2023 - Set 2023 (6 meses)"
+);
+
+hoverChangeExperience(
+  ".codigofontetv",
+  `Trabalhei como Social Media e Designer no Código Fonte TV, 
+  onde teve como foco as criações de conteúdos sobre programação 
+  para a comunidade dev através das redes sociais.`,
+  "Social Media e Designer",
+  "Código Fonte TV",
+  "Jun 2021 - Jan 2022 (8 meses)"
+);
+
+hoverChangeExperience(
+  ".contweb",
+  `Realizei o estágio na ContWeb, uma empresa de contabilidade. 
+  Onde pela primeira vez tive experiência com o React. Além do
+  desenvolvimento frontend criei os designs da nova plataforma da empresa.`,
+  "Developer frontend e UI Designer",
+  "ContWeb",
+  "Set 2021 - Nov 2021 (3 meses)"
+);
+
+hoverChangeDescription(
+  ".html",
+  "Power BI: Ferramenta da Microsoft para análise de dados que permite criar dashboards interativos e relatórios para visualizar informações de diferentes fontes de dados."
+);
+hoverChangeDescription(
+  ".css",
+  "Tableau: Plataforma de visualização e análise de dados que facilita a criação de gráficos, dashboards e mapas interativos para descobrir insights."
+);
+hoverChangeDescription(
+  ".js",
+  "Looker: Uma plataforma de inteligência de negócios que permite explorar, visualizar e compartilhar insights de dados em toda a organização, com foco em facilitar a colaboração e a tomada de decisões baseadas em dados."
+);
+hoverChangeDescription(
+  ".sass",
+  "Python: Uma linguagem de programação de alto nível, conhecida por sua simplicidade e facilidade de uso, amplamente utilizada em ciência de dados, análise de dados, desenvolvimento web, automação, entre outros."
+);
+hoverChangeDescription(
+  ".react",
+  "SQL: Linguagem de consulta estruturada usada para gerenciar e manipular bancos de dados relacionais, permitindo a recuperação, inserção, atualização e exclusão de dados."
+);
+hoverChangeDescription(
+  ".next",
+  "MongoDB: Um banco de dados NoSQL orientado a documentos, que armazena dados em documentos JSON flexíveis, oferecendo escalabilidade e desempenho para aplicações modernas."
+);
+hoverChangeDescription(
+  ".styled",
+  "Google Sheets: Uma ferramenta de planilha baseada na web, oferecida pelo Google como parte do Google Drive, permitindo colaboração em tempo real e acesso fácil aos dados a partir de qualquer dispositivo conectado à internet."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "SAP: Sistema integrado de gestão empresarial que ajuda as empresas a gerenciar processos em áreas como finanças, vendas, logística e recursos humanos, unificando informações em uma única plataforma."
+);
+hoverChangeDescription(
+  ".typescript",
+  "DataBricks: Uma plataforma de análise de big data que simplifica o processamento de grandes volumes de dados, oferecendo ferramentas para análise, processamento e colaboração em ambientes de big data."
+);
+hoverChangeDescription(
+  ".radix",
+  "Streamlit: Uma biblioteca de código aberto em Python usada para criar aplicativos da web interativos para análise de dados e machine learning de forma rápida e fácil, permitindo aos usuários desenvolver interfaces intuitivas e visualizações interativas diretamente a partir de scripts Python."
+);
+hoverChangeDescription(
+  ".cypress",
+  "Java: Linguagem de programação multiplataforma, amplamente usada em desenvolvimento de software e aplicativos corporativos pela sua portabilidade e segurança."
+);
+hoverChangeDescription(
+  ".storybook",
+  "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+);
